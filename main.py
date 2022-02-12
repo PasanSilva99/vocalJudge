8e0df3fb7b08c24011373025978170320de2b093
Pasan Anuththara <pasananuththara19@gmail.com>
2022-02-12 22:48:41 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+import librosa
+
+import librosa.display
+
+
+def print_hi(name):
+    print(f'Hi, {name}')
+
+
+if __name__ == '__main__':
+    print_hi('PyCharm')
